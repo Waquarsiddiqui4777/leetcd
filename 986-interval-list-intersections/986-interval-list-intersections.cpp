99ec684b478e62ef03157c840f53d1491ce42da0
Waquarsiddiqui4777 <75410367+Waquarsiddiqui4777@users.noreply.github.com>
2022-09-15 03:38:42 -0700

    Time: 85 ms (12.45%), Space: 18.8 MB (40.97%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& l1, vector<vector<int>>& l2) {
+       int n = l1.size(), m = l2.size(), i=0, j=0;
+			if(!n || !m) return {};
+			vector<vector<int>> ans;
+			while(i<n && j<m)
+			{
+				int l = max(l1[i][0],l2[j][0]);
+				int r = min(l1[i][1],l2[j][1]);
+				if(l<=r)    ans.push_back({l,r});
+				if(l1[i][1]<=l2[j][1])   i++;
+				else    j++;
+			}
+			return ans; 
+    }
+};
